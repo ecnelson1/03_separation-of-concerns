e4758221443aa79353fa9efb8c5e04974a1bb44d
Eion Nelson <ecnelson1@live.com>
2021-03-28 15:04:55 -0700

    wrote initial CRUD routes

--- demo/lib/controllers/orders.js
+++ demo/lib/controllers/orders.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const OrderService = require('../services/OrderService');
+const pool = require('../utils/pool');
 
 module.exports = Router()
   .post('/', async (req, res, next) => {
@@ -12,15 +13,41 @@ module.exports = Router()
   })
   .get('/', async (req, res, next) => {
     try{
-      const orders = 
+      const orders = await pool.query('SELECT * FROM orders')
+      res.json(orders.rows);
+    } catch (err) {
+      next(err);
     }
   })
+
   .get('/:id', async (req, res, next) => {
-
+    try { 
+    const order = await pool.query('SELECT * FROM orders WHERE id=$1', [id]);
+    res.json(order.rows);
+    } catch (err) {
+      next(err);
+    }
   })
+
   .put('/:id', async (req, res, next) => {
-
+    try {
+      const updatedOrder = await pool.query(`
+      UPDATE orders
+      SET quantity = $1
+      WHERE id = $2
+      RETURNING *
+       `,
+       [order.quantity, id]
+       );
+    } catch (err) {
+      next(err);
+    }
   })
-  .delete('/:id', async (req, res, next) => {
 
+  .delete('/:id', async (req, res, next) => {
+    try{
+      const deletedOrder = await pool.query(`DELETE FROM orders WHERE id=$1 RETURNING *`, [id]) 
+    } catch (err) {
+      next(err);
+    }
   });
