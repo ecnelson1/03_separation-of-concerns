d66655b953472909a829bc4cd1cda6b1ebae1347
Eion Nelson <ecnelson1@live.com>
2021-03-29 10:58:46 -0700

    debugged server issues and tests to pass all CI

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -30,49 +30,52 @@ describe('createOrder route', () => {
 describe('getOrders route', () =>{
   it('returns all the orders in the database', () => {
     return request(app)
-    .get('api/v1/orders')
-    .then((res) => {
-      expect(res.body).toEqual({
+      .get('/api/v1/orders')
+      .then((res) => {
+        expect(res.body).toEqual([{
           id: '1',
           quantity: 10,
-        })
-    })
-  })
-})
+        }]);
+      });
+  });
+});
 
 describe('getOrderById route', () =>{
   it('returns all the orders in the database for the given Id', () => {
     return request(app)
-    .get('api/v1/orders/1')
-    .then((res) => {
-      expect(res.body).toEqual({
+      .get('/api/v1/orders/1')
+      .then((res) => {
+        expect(res.body).toEqual({
           id: '1',
           quantity: 10,
-        })
-    })
-  })
-})
+        });
+      });
+  });
+});
 
-describe('updateOrder route', () =>{
+describe('updateOrder route', () => {
   it('updates the order quantity in the database for the given Id and sends a text message', () => {
     return request(app)
-    .put('api/v1/orders/1')
-    .send({quantity: 100})
-    .then((res) => {
-      expect(res.body).toEqual({
+      .put('/api/v1/orders/1')
+      .send({ quantity: 100 })
+      .then((res) => {
+        expect(res.body).toEqual({
           id: '1',
           quantity: 100,
-        })
-    })
-  })
-})
+        });
+      });
+  });
+});
 
 describe(' deleteOrderById route', () =>{
   it('deletes the order in the database for the given Id', () => {
     return request(app)
-    .delete('api/v1/orders/1')
-    .then((res) => {
-      expect(res.body).toEqual([])
-    })
-  })
-})
+      .delete('/api/v1/orders/1')
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          quantity: 100,
+        }); //deleted item
+      });
+  });
+});
